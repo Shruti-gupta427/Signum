be0193ed1da3a2d42dbcb787865e23ea9cbef091
shrutiXcode <sg920450@gmail.com>
2026-05-29 15:08:01 +0530

    feat: add SSE channel

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 require('dotenv').config();
 const { connect } = require('./src/db');
-const { sendEmail } = require('./src/channels/email');
 
 const start = async () => {
   await connect();
-
-  const result = await sendEmail({
-    to: 'bcs_2024068@iiitm.ac.in',
-    subject: 'Signum Test Email',
-    body: 'Signal everything, miss nothing.'
-  });
-
-  console.log(result);
 };
 
 start();
