c9a2db47b171b9e5d92d30e9fceb862bcf65d252
shrutiXcode <sg920450@gmail.com>
2026-06-04 16:25:54 +0530

    added initial page with testing guide

--- src/routes/server.js
+++ src/routes/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const notifyRoutes = require('./notify');
 const subscribeRoutes = require('./subscribe');
 const statusRoutes = require('./status');
 const createServer = () => {
   const app = express();
-  app.use(express.json());  
+  app.use(express.static(path.join(__dirname, '../../public')));
+  app.use(express.json());
   app.use('/notify',notifyRoutes );
   app.use('/subscribe',subscribeRoutes);
   app.use('/notifications', statusRoutes);
